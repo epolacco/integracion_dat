a131539f971aaee18be1b60e6924f53b2d1addc8
epolacco <168129464+epolacco@users.noreply.github.com>
2024-05-07 10:31:30 -0300

    Add files via upload

---
+++ main_page.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.markdown("# Integración a DAT")
+st.subheader("Bienvenido a DAT CREA - Procesamiento de datos")
+st.write("Aquí podrás incorporar los datos de tu aplicación al *formato DAT* :sunglasses:")
+st.write("INSTRUCCIONES\n1. Seleccioná una plataforma del menú de la izquierda.\n2. Una vez ahí, seleccioná la estacionalidad del cultivo.\n3. Apretá el botón 'PROCESAR'.\n4. Si está todo OK, apretá 'Descargar' y elegí la carpeta donde guardar el archivo.")
