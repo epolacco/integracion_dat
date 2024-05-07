13d8aab8bdf06bd10003d90c350843ea887163ac
epolacco <168129464+epolacco@users.noreply.github.com>
2024-05-07 13:55:56 -0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 st.markdown("# Integración a DAT")
 st.subheader("Bienvenido a DAT CREA - Procesamiento de datos")
-st.write("Aquí podrás incorporar los datos de tu aplicación al *formato DAT* :sunglasses:")
+st.write("Acá podrás incorporar los datos de tu aplicación al *formato DAT* :sunglasses:")
 st.write("INSTRUCCIONES\n1. Seleccioná una plataforma del menú de la izquierda.\n2. Una vez ahí, seleccioná la estacionalidad del cultivo.\n3. Apretá el botón 'PROCESAR'.\n4. Si está todo OK, apretá 'Descargar' y elegí la carpeta donde guardar el archivo.")
